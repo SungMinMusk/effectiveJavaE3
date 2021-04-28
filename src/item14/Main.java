ce92faf050b217d0b6251d674c427459a91579d7
WIN-AS8B4CQUG4J\KSM <sm3487@naver.com>
2021-04-28 19:54:11 +0900

    item14

---
+++ src/item14/Main.java
@@ -0,0 +1,11 @@
+package item14;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Point a = new Point(1,1);
+        Point b = new Point(1,2);
+
+        System.out.println(a.compareTo(b));
+    }
+}
---
+++ src/item14/Point.java
@@ -0,0 +1,32 @@
+package item14;
+
+import java.util.Comparator;
+
+import static java.util.Comparator.comparingInt;
+
+public class Point implements Comparable<Point> {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+//    @Override
+//    public int compareTo(Point point) {
+//        int result = Integer.compare(x, point.x);
+//        if (result == 0) {
+//            return Integer.compare(y, point.y);
+//        }
+//        return result;
+//    }
+
+    @Override
+    public int compareTo(Point p) {
+        return COMPARATOR.compare(this, p);
+    }
+    private static final Comparator<Point> COMPARATOR =
+            comparingInt((Point p) -> p.x)
+            .thenComparingInt((Point p) -> p.y);
+}
